ed4958b945f734f9c930f6dbe5c152153d1dc689
MavelSterling <mavelynsl@gmail.com>
2022-03-21 18:09:35 -0500

    Update UsuarioDAO.java

--- univalleCompany/src/dao/UsuarioDAO.java
+++ univalleCompany/src/dao/UsuarioDAO.java
@@ -6,7 +6,9 @@ package dao;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import javax.swing.JOptionPane;
 import model.Usuario;
 import servicios.Fachada;
@@ -94,7 +96,91 @@ public class UsuarioDAO {
         return rtdo;
     }
     
+    /**
+     * 
+     * @param codigo código del programa a borrar
+     * @return rtdo resultado de la operación borrar
+     */
+    public int borrarPrograma(String Nit){      
+        Connection con = null;
+        PreparedStatement pstm = null;
+        int rtdo;
+        rtdo = 0;
+        try{
+            con = Fachada.getConnection();
+            String sql = "DELETE FROM Usuario WHERE Nit = ? ";
+            pstm = con.prepareStatement(sql);
+            pstm.setString(1, Nit);
+            rtdo = pstm.executeUpdate(); 
+            return rtdo;
+        }
+        catch(SQLException ex){
+            JOptionPane.showMessageDialog(null,"Nit : " + 
+                        ex.getErrorCode() + "\nError :" + ex.getMessage());
+        } 
+        finally{
+            try{
+                if(pstm!=null) pstm.close();                
+            }
+            catch(SQLException ex){
+                JOptionPane.showMessageDialog(null,"Nit : " + 
+                        ex.getErrorCode() + "\nError :" + ex.getMessage());
+            }
+        }
+        return rtdo;
+    }
     
-    
+    /**
+     * 
+     * @param codigo codigo del programa a listar, 0 se listaran todos
+     * @return ArrayList, lista de objetos Programa
+     */
+    public ArrayList<Usuario> listadoProgramas(String Nit){      
+        Connection con = null;
+        PreparedStatement pstm = null;
+        ResultSet rs = null;
+        ArrayList<Usuario> listado = new ArrayList<>();
+        try{
+            con = Fachada.getConnection();
+            String sql="";
+            if(Nit.equalsIgnoreCase("0")){
+                sql = "SELECT * FROM programa ORDER BY codigo";            
+            }else{
+                sql = "SELECT * FROM programa where codigo = ? "
+                    + "ORDER BY codigo";      
+            }                        
+            pstm = con.prepareStatement(sql);
+            
+            if(Nit != "0"){
+                pstm.setString(1, Nit);
+            }
+            
+            rs = pstm.executeQuery();
+                        
+            Usuario usuario = null;
+            while(rs.next()){
+                usuario = new Usuario();
+                usuario.setNombre(rs.getString("Nombre"));
+                usuario.setDireccion(rs.getString("Direccion"));
+                usuario.setNit(rs.getInt("Nit"));
+                listado.add(usuario);
+            }
+        }
+        catch(SQLException ex){
+            JOptionPane.showMessageDialog(null,"Nit : " + 
+                        ex.getErrorCode() + "\nError :" + ex.getMessage());
+        }
+        finally{
+            try{
+                if(rs!=null) rs.close();
+                if(pstm!=null) pstm.close();                
+            }
+            catch(SQLException ex){
+                JOptionPane.showMessageDialog(null,"Nit : " + 
+                        ex.getErrorCode() + "\nError :" + ex.getMessage());
+            }
+        }
+        return listado;
+    }
     
 }
