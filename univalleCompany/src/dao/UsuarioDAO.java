f063e0fa780857f9e3b3ab798208200d192dae4b
MavelSterling <mavelynsl@gmail.com>
2022-03-21 16:13:19 -0500

    clase usuarioDAO

--- univalleCompany/src/dao/UsuarioDAO.java
+++ univalleCompany/src/dao/UsuarioDAO.java
@@ -2,12 +2,13 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package model;
+package dao;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import javax.swing.JOptionPane;
+import model.Usuario;
 import servicios.Fachada;
 
 /**
