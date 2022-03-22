8e90091bbe19ef5308c40d294e0967e82f9703ee
MavelSterling <mavelynsl@gmail.com>
2022-03-21 19:36:27 -0500

    Update ProveedorDAO.java

--- univalleCompany/src/dao/ProveedorDAO.java
+++ univalleCompany/src/dao/ProveedorDAO.java
@@ -106,7 +106,7 @@ public class ProveedorDAO {
      * @param codigo código del programa a borrar
      * @return rtdo resultado de la operación borrar
      */
-    public int borrarPrograma(String Nit){      
+    public int borrarProveedor(String Nit){      
         Connection con = null;
         PreparedStatement pstm = null;
         int rtdo;
@@ -140,7 +140,7 @@ public class ProveedorDAO {
      * @param codigo codigo del programa a listar, 0 se listaran todos
      * @return ArrayList, lista de objetos Programa
      */
-    public ArrayList<Proveedor> listadoProgramas(String Nit){      
+    public ArrayList<Proveedor> listadoProveedor(String Nit){      
         Connection con = null;
         PreparedStatement pstm = null;
         ResultSet rs = null;
