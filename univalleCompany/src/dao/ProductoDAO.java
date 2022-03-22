6b440514404cb50f4cca9b0405a98464a1949ac2
MavelSterling <mavelynsl@gmail.com>
2022-03-21 22:02:33 -0500

    Create ProductoDAO.java

---
+++ univalleCompany/src/dao/ProductoDAO.java
@@ -0,0 +1,198 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import javax.swing.JOptionPane;
+import model.Cliente;
+import model.Producto;
+import servicios.Fachada;
+
+/**
+ *
+ * @author mavel
+ */
+public class ProductoDAO {
+     public ProductoDAO() {
+    }
+    
+    /**
+     * 
+     * @param p Objeto de la clase Programa a grabar
+     * @return rtdo resultado de la operación grbar
+     */
+    public int grabarProducto(Producto prod){      
+        Connection con = null;
+        PreparedStatement pstm;
+        pstm = null;
+        int rtdo;
+        rtdo = 0;
+        try{
+            con = Fachada.getConnection();
+            String sql = "INSERT INTO producto values (?,?,?,?,?)";
+            pstm = con.prepareStatement(sql);
+            pstm.setInt(1, prod.getIdProductos());
+            pstm.setString(2, prod.getNombreProd());
+            pstm.setDouble(3,prod.getPrecioUnitario());
+            pstm.setString(4, prod.getDescripcion());
+            pstm.setInt(5, prod.getCantidadProd());
+            rtdo = pstm.executeUpdate();  
+        }
+        catch(SQLException ex){
+            JOptionPane.showMessageDialog(null,"idProductos : " + 
+                        ex.getErrorCode() + "\nError :" + ex.getMessage());
+        }
+        finally{
+            try{
+                if(pstm!=null) pstm.close();                
+            }
+            catch(SQLException ex){
+                JOptionPane.showMessageDialog(null,"idProductos : " + 
+                        ex.getErrorCode() + "\nError :" + ex.getMessage());
+            }
+        }
+        return rtdo;
+    }
+    
+    /**
+     * 
+     * @param p Objeto de la clase Programa a grabar
+     * @return rtdo resultado de la operación modificar
+     */
+    public int modificarProducto(Producto prod){      
+        Connection con = null;
+        PreparedStatement pstm;
+        pstm = null;
+        int rtdo;
+        rtdo = 0;
+        try{
+            con = Fachada.getConnection();
+            String sql = "UPDATE producto " +
+                         "SET NombreProducto = ?, Precio = ?, Descripcion = ? "
+                    + " cantidad = ? "
+                    +    "WHERE idProductos=?";
+            pstm = con.prepareStatement(sql);            
+            pstm.setInt(1, prod.getIdProductos());
+            pstm.setString(2, prod.getNombreProd());
+            pstm.setDouble(3,prod.getPrecioUnitario());
+            pstm.setString(4, prod.getDescripcion());
+            pstm.setInt(5, prod.getCantidadProd());
+            rtdo = pstm.executeUpdate();  
+        }
+        catch(SQLException ex){
+            JOptionPane.showMessageDialog(null,"idProductos : " + 
+                        ex.getErrorCode() + "\nError :" + ex.getMessage());
+        }
+        finally{
+            try{
+                if(pstm!=null) pstm.close();                
+            }
+            catch(SQLException ex){
+                JOptionPane.showMessageDialog(null,"idProductos : " + 
+                        ex.getErrorCode() + "\nError :" + ex.getMessage());
+            }
+        }
+        return rtdo;
+    }
+    
+    
+    /**
+     * 
+     * @param codigo código del programa a borrar
+     * @return rtdo resultado de la operación borrar
+     */
+    public int borrarProducto(String id){      
+        Connection con = null;
+        PreparedStatement pstm = null;
+        int rtdo;
+        rtdo = 0;
+        try{
+            con = Fachada.getConnection();
+            String sql = "DELETE FROM producto WHERE idProductos = ? ";
+            pstm = con.prepareStatement(sql);
+            pstm.setString(1, id);
+            rtdo = pstm.executeUpdate(); 
+            return rtdo;
+        }
+        catch(SQLException ex){
+            JOptionPane.showMessageDialog(null,"idProductos : " + 
+                        ex.getErrorCode() + "\nError :" + ex.getMessage());
+        } 
+        finally{
+            try{
+                if(pstm!=null) pstm.close();                
+            }
+            catch(SQLException ex){
+                JOptionPane.showMessageDialog(null,"idProductos : " + 
+                        ex.getErrorCode() + "\nError :" + ex.getMessage());
+            }
+        }
+        return rtdo;
+    }
+
+    
+    /**
+     * 
+     * @param codigo codigo del programa a listar, 0 se listaran todos
+     * @return ArrayList, lista de objetos Programa
+     */
+    public ArrayList<Producto> listadoCliente(String id){      
+        Connection con = null;
+        PreparedStatement pstm = null;
+        ResultSet rs = null;
+        ArrayList<Producto> listado = new ArrayList<>();
+        try{
+            con = Fachada.getConnection();
+            String sql="";
+            if(id.equalsIgnoreCase("0")){
+                sql = "SELECT * FROM producto ORDER BY idProductos";            
+            }else{
+                sql = "SELECT * FROM producto where idProductos = ? "
+                    + "ORDER BY idProductos";      
+            }                        
+            pstm = con.prepareStatement(sql);
+            
+            if(id != "0"){
+                pstm.setString(1, id);
+            }
+            
+            rs = pstm.executeQuery();
+                        
+            Producto producto = null;
+            while(rs.next()){
+                producto = new Producto();
+                producto.setIdProductos(rs.getInt("idProductos"));
+                producto.setNombreProd(rs.getString("NombreProducto"));
+                producto.setPrecioUnitario(rs.getDouble("Precio"));
+                producto.setDescripcion(rs.getString("Descripcion")); 
+                producto.setCantidadProd(rs.getInt("cantidad")); 
+                listado.add(producto);
+            }
+        }
+        catch(SQLException ex){
+            JOptionPane.showMessageDialog(null,"idProductos : " + 
+                        ex.getErrorCode() + "\nError :" + ex.getMessage());
+        }
+        finally{
+            try{
+                if(rs!=null) rs.close();
+                if(pstm!=null) pstm.close();                
+            }
+            catch(SQLException ex){
+                JOptionPane.showMessageDialog(null,"idProductos : " + 
+                        ex.getErrorCode() + "\nError :" + ex.getMessage());
+            }
+        }
+        return listado;
+    }
+    
+     
+     
+     
+}
