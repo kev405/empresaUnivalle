705d69e3d2a43d0d5554e71e8bb0d1fb505158b4
MavelSterling <mavelynsl@gmail.com>
2022-03-23 10:21:13 -0500

    Update ComercializacionDAO.java

--- univalleCompany/src/dao/ComercializacionDAO.java
+++ univalleCompany/src/dao/ComercializacionDAO.java
@@ -35,21 +35,24 @@ public class ComercializacionDAO {
         rtdo = 0;
         try{
             con = Fachada.getConnection();
-            String sql = "INSERT INTO Comercializacion values (?,?,?,?,?,?, ?)";
+            String sql = "INSERT INTO comercializacion values (?,?,?,?,?,?,?,?)";
             pstm = con.prepareStatement(sql);
             pstm.setInt(1, comerc.getIdCliente_Com());
-            pstm.setString(2, comerc.getNombreProductos_Com());
-            pstm.setDouble(3,comerc.getPrecio_Com());
-            pstm.setString(4, comerc.getNombreProductos_Com());
-            pstm.setString(5, comerc.getVendedor());
-            pstm.setInt(6, comerc.getCantidad_Com());
-            pstm.setString(7, comerc.getDireccionVenta());
+            pstm.setDouble(2,comerc.getPrecio_Com());
+            pstm.setString(3, comerc.getVendedor());
+            pstm.setInt(4, comerc.getCantidad_Com());
+            pstm.setInt(5, comerc.getNumeroFactura());
+            pstm.setString(6, comerc.getDireccionVenta());
+            pstm.setInt(7, comerc.getComision_venta());
+            pstm.setInt(8, comerc.getId_producto_com());
+
+
 
 
             rtdo = pstm.executeUpdate();  
         }
         catch(SQLException ex){
-            JOptionPane.showMessageDialog(null,"idCliente_Com : " + 
+            JOptionPane.showMessageDialog(null,"numerofactura : " + 
                         ex.getErrorCode() + "\nError :" + ex.getMessage());
         }
         finally{
@@ -57,7 +60,7 @@ public class ComercializacionDAO {
                 if(pstm!=null) pstm.close();                
             }
             catch(SQLException ex){
-                JOptionPane.showMessageDialog(null,"idCliente_Com : " + 
+                JOptionPane.showMessageDialog(null,"numerofactura : " + 
                         ex.getErrorCode() + "\nError :" + ex.getMessage());
             }
         }
@@ -78,23 +81,24 @@ public class ComercializacionDAO {
         rtdo = 0;
         try{
             con = Fachada.getConnection();
-            String sql = "UPDATE Comercializacion " +
-                         "SET nombreProductos_Com = ?, precio_Com = ?, "
-                    + "vendedor = ?, cantidad_Com= ?, numeroFactura = ?,  "
-                    +    "WHERE idCliente_Com=?";
+            String sql = "UPDATE comercializacion " +
+                         "SET idcliente_com = ?, precio_com = ?,vendedor=? "
+                    + " cantidad_com= ?, direccionventa = ?,"
+                    + "comision_venta=? , id_producto_com=? "
+                    +    "WHERE numerofactura=?";
             pstm = con.prepareStatement(sql);            
-             pstm.setInt(1, comerc.getIdCliente_Com());
-            pstm.setString(2, comerc.getNombreProductos_Com());
-            pstm.setDouble(3,comerc.getPrecio_Com());
-            pstm.setString(4, comerc.getNombreProductos_Com());
-            pstm.setString(5, comerc.getVendedor());
-            pstm.setInt(6, comerc.getCantidad_Com());
-            pstm.setString(7, comerc.getDireccionVenta());
-
+            pstm.setInt(1, comerc.getIdCliente_Com());
+            pstm.setDouble(2,comerc.getPrecio_Com());
+            pstm.setString(3, comerc.getVendedor());
+            pstm.setInt(4, comerc.getCantidad_Com());
+            pstm.setInt(5, comerc.getNumeroFactura());
+            pstm.setString(6, comerc.getDireccionVenta());
+            pstm.setInt(7, comerc.getComision_venta());
+            pstm.setInt(8, comerc.getId_producto_com());
             rtdo = pstm.executeUpdate();  
         }
         catch(SQLException ex){
-            JOptionPane.showMessageDialog(null,"idCliente_Com : " + 
+            JOptionPane.showMessageDialog(null,"numerofactura : " + 
                         ex.getErrorCode() + "\nError :" + ex.getMessage());
         }
         finally{
@@ -102,7 +106,7 @@ public class ComercializacionDAO {
                 if(pstm!=null) pstm.close();                
             }
             catch(SQLException ex){
-                JOptionPane.showMessageDialog(null,"idCliente_Com : " + 
+                JOptionPane.showMessageDialog(null,"numerofactura : " + 
                         ex.getErrorCode() + "\nError :" + ex.getMessage());
             }
         }
@@ -123,14 +127,14 @@ public class ComercializacionDAO {
         rtdo = 0;
         try{
             con = Fachada.getConnection();
-            String sql = "DELETE FROM Comercializacion WHERE idCliente_Com = ? ";
+            String sql = "DELETE FROM Comercializacion WHERE numerofactura = ? ";
             pstm = con.prepareStatement(sql);
             pstm.setString(1, id);
             rtdo = pstm.executeUpdate(); 
             return rtdo;
         }
         catch(SQLException ex){
-            JOptionPane.showMessageDialog(null,"idCliente_Com : " + 
+            JOptionPane.showMessageDialog(null,"numerofactura : " + 
                         ex.getErrorCode() + "\nError :" + ex.getMessage());
         } 
         finally{
@@ -138,7 +142,7 @@ public class ComercializacionDAO {
                 if(pstm!=null) pstm.close();                
             }
             catch(SQLException ex){
-                JOptionPane.showMessageDialog(null,"idCliente_Com : " + 
+                JOptionPane.showMessageDialog(null,"numerofactura : " + 
                         ex.getErrorCode() + "\nError :" + ex.getMessage());
             }
         }
@@ -160,10 +164,10 @@ public class ComercializacionDAO {
             con = Fachada.getConnection();
             String sql="";
             if(id.equalsIgnoreCase("0")){
-                sql = "SELECT * FROM Comercializacion ORDER BY idCliente_Com";            
+                sql = "SELECT * FROM comercializacion ORDER BY numerofactura";            
             }else{
-                sql = "SELECT * FROM Comercializacion where idCliente_Com = ? "
-                    + "ORDER BY idCliente_Com";      
+                sql = "SELECT * FROM comercializacion where numerofactura = ? "
+                    + "ORDER BY numerofactura";      
             }                        
             pstm = con.prepareStatement(sql);
             
@@ -176,19 +180,21 @@ public class ComercializacionDAO {
             Comercializacion comercializacion = null;
             while(rs.next()){
                 comercializacion = new Comercializacion();
-                comercializacion.setIdCliente_Com(rs.getInt("idCliente_Com"));
-                comercializacion.setNombreProductos_Com(rs.getString("Producto_Com"));
-                comercializacion.setPrecio_Com(rs.getDouble("Precio_Com"));
-                comercializacion.setNumeroFactura(rs.getInt("NumeroFactura"));
-                comercializacion.setVendedor(rs.getString("Vendedor"));
-                comercializacion.setCantidad_Com(rs.getInt("Cantidad_Com")); 
-                comercializacion.setDireccionVenta(rs.getString("DireccionVenta"));
+                comercializacion.setIdCliente_Com(rs.getInt("idcliente_com"));
+                comercializacion.setPrecio_Com(rs.getDouble("precio_com"));
+                comercializacion.setVendedor(rs.getString("vendedor"));
+                comercializacion.setNumeroFactura(rs.getInt("numerofactura"));
+                comercializacion.setCantidad_Com(rs.getInt("cantidad_com")); 
+                comercializacion.setDireccionVenta(rs.getString("direccionventa"));
+                comercializacion.setComision_venta(rs.getInt("comision_venta"));
+                comercializacion.setId_producto_com(rs.getInt("id_producto_com"));
+
 
                 listado.add(comercializacion);
             }
         }
         catch(SQLException ex){
-            JOptionPane.showMessageDialog(null,"idCliente_Com : " + 
+            JOptionPane.showMessageDialog(null,"numerofactura : " + 
                         ex.getErrorCode() + "\nError :" + ex.getMessage());
         }
         finally{
@@ -197,7 +203,7 @@ public class ComercializacionDAO {
                 if(pstm!=null) pstm.close();                
             }
             catch(SQLException ex){
-                JOptionPane.showMessageDialog(null,"idCliente_Com : " + 
+                JOptionPane.showMessageDialog(null,"numerofactura : " + 
                         ex.getErrorCode() + "\nError :" + ex.getMessage());
             }
         }
